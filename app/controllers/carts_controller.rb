21f2bc32d9e0901f3cd458043581873f711f1e30
Justine <jjtroyke@gmail.com>
2018-05-30 12:50:17 -0600

    WIP cart on accessory show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,9 +3,11 @@ class CartsController < ApplicationController
 
   def create
     item = Item.find(params[:item_id])
+    item_id = item.id.to_s
+    session[:cart][item_id] = session[:cart][item_id] + 1
     @cart.add_item(item)
     session[:cart] = @cart.contents
-    flash[:notice] = "#{item.title} has been add to your cart!"
+    flash[:notice] = "#{}#{item.title} has been add to your cart!"
     redirect_to accessories_path
   end
 end
